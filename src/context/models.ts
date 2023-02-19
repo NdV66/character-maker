8f0919aaf5a92d5432378fcd979886a35ea69fd9
Marta Zazlak <marta.zazlak@hotmail.com>
2023-02-19 14:52:54 +0100

    update model manager

--- src/context/models.ts
+++ src/context/models.ts
@@ -2,9 +2,13 @@ import { ModelsManager } from '../models';
 
 import { Models } from '../types';
 import { IAppGeneralSettings } from '../types/interfaces';
+import { ICharacterTraitsManager } from '../types/interfaces/ICharacterTraitsManager';
 
 const allModels = {
     [Models.APP_GENERAL_SETTINGS]: ModelsManager.getSingleton<IAppGeneralSettings>(Models.APP_GENERAL_SETTINGS),
+    [Models.CHARACTER_TRAITS_MANAGER]: ModelsManager.getSingleton<ICharacterTraitsManager>(
+        Models.CHARACTER_TRAITS_MANAGER,
+    ),
 };
 
 export const getModelByKey = <T>(key: Models) => allModels[key] as any as T;
--- src/types/models.ts
+++ src/types/models.ts
@@ -1,3 +1,4 @@
 export const enum Models {
     APP_GENERAL_SETTINGS = 'GeneralAppSettingsModel',
+    CHARACTER_TRAITS_MANAGER = 'CharacterTraitsManagerModel',
 }
