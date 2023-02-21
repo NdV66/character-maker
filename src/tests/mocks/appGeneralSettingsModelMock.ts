3fca74728b76c38d90aedf749c6bd9c8d46d7077
Marta Zazlak <marta.zazlak@hotmail.com>
2023-02-21 11:57:40 +0100

    ref

--- src/tests/mocks/appGeneralSettingsModelMock.ts
+++ src/tests/mocks/appGeneralSettingsModelMock.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+import { AppLangs, AppTheme, TTheme, TTranslations } from '../../types';
 import { appLangModelMock } from './appLangModelMock';
 import { appThemeModelMock } from './appThemeModelMock';
 
@@ -6,4 +8,17 @@ export const appGeneralSettingsModelMock = () => ({
     appLangModel: appLangModelMock(),
 
     setDefaultValues: jest.fn(),
+});
+
+export const appContextViewModelMock = () => ({
+    theme$: new Observable<TTheme>(),
+    appTheme$: new Observable<AppTheme>(),
+    translations$: new Observable<TTranslations>(),
+    isLoading$: new Observable<boolean>(),
+    appLang$: new Observable<AppLangs>(),
+
+    setDefaultValues: jest.fn(),
+    setIsLoading: jest.fn(),
+    changeAppLang: jest.fn(),
+    toggleAppTheme: jest.fn(),
 });
--- src/tests/models/changeLangElementViewModel.test.ts
+++ src/tests/models/changeLangElementViewModel.test.ts
@@ -1,66 +1,64 @@
-// import { AppLangs, IAppGeneralSettings, TTranslationsLang } from '../../types';
-// import { appGeneralSettingsModelMock } from '../mocks';
-// import * as modelsTools from '../../context/models';
-// import { Observable } from 'rxjs';
-// import { TestScheduler } from 'rxjs/testing';
-// import { ChangeLangElementViewModel } from '../../models/viewModels/ChangeLangElementViewModel';
-// import { TEXTS_EN } from '../../langs/en';
+import { AppLangs, IAppContextViewModel, TTranslationsLang } from '../../types';
+import { appContextViewModelMock } from '../mocks';
+import { Observable } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+import { ChangeLangElementViewModel } from '../../models/viewModels/ChangeLangElementViewModel';
+import { TEXTS_EN } from '../../langs/en';
 
-// describe('AppContextViewModel', () => {
-//     let generalSettingsModelMock: IAppGeneralSettings;
-//     let testScheduler: TestScheduler;
+describe('AppContextViewModel', () => {
+    let appContextViewModelMok: IAppContextViewModel;
+    let testScheduler: TestScheduler;
 
-//     beforeEach(() => {
-//         generalSettingsModelMock = appGeneralSettingsModelMock() as any as IAppGeneralSettings;
-//         jest.spyOn(modelsTools, 'getModelByKey').mockReturnValue(generalSettingsModelMock);
-//         testScheduler = new TestScheduler((actual, expected) => {
-//             expect(actual).toEqual(expected);
-//         });
-//     });
+    beforeEach(() => {
+        appContextViewModelMok = appContextViewModelMock() as any as IAppContextViewModel;
+        testScheduler = new TestScheduler((actual, expected) => {
+            expect(actual).toEqual(expected);
+        });
+    });
 
-//     test('Should return current appLang$', () => {
-//         const lang = AppLangs.PL;
-//         generalSettingsModelMock.appLangModel.appLang = new Observable((observer) => observer.next(lang));
+    test('Should return current appLang$', () => {
+        const lang = AppLangs.PL;
+        appContextViewModelMok.appLang$ = new Observable((observer) => observer.next(lang));
 
-//         testScheduler.run(({ expectObservable }) => {
-//             const model = new ChangeLangElementViewModel();
-//             expectObservable(model.appLang$).toBe('a', { a: lang });
-//         });
-//     });
+        testScheduler.run(({ expectObservable }) => {
+            const model = new ChangeLangElementViewModel(appContextViewModelMok);
+            expectObservable(model.appLang$).toBe('a', { a: lang });
+        });
+    });
 
-//     test('Should call changeAppLang', () => {
-//         const lang = AppLangs.EN;
-//         const model = new ChangeLangElementViewModel();
-//         model.changeAppLang(lang);
+    test('Should call changeAppLang', () => {
+        const lang = AppLangs.EN;
+        const model = new ChangeLangElementViewModel(appContextViewModelMok);
+        model.changeAppLang(lang);
 
-//         expect(generalSettingsModelMock.appLangModel.changeAppLang).toHaveBeenCalledTimes(1);
-//         expect(generalSettingsModelMock.appLangModel.changeAppLang).toHaveBeenCalledWith(lang);
-//     });
+        expect(appContextViewModelMok.changeAppLang).toHaveBeenCalledTimes(1);
+        expect(appContextViewModelMok.changeAppLang).toHaveBeenCalledWith(lang);
+    });
 
-//     test('Should map to item correctly', () => {
-//         const lang: TTranslationsLang = {
-//             label: 'Polski',
-//             value: AppLangs.PL,
-//         };
-//         const expectedResult = { key: lang.value, label: lang.label };
-//         const model = new ChangeLangElementViewModel();
+    test('Should map to item correctly', () => {
+        const lang: TTranslationsLang = {
+            label: 'Polski',
+            value: AppLangs.PL,
+        };
+        const expectedResult = { key: lang.value, label: lang.label };
+        const model = new ChangeLangElementViewModel(appContextViewModelMok);
 
-//         const result = model['_mapToItem'](lang);
-//         expect(result).toEqual(expectedResult);
-//     });
+        const result = model['_mapToItem'](lang);
+        expect(result).toEqual(expectedResult);
+    });
 
-//     test('Should prepare items$ correctly', () => {
-//         const expectedResult = [
-//             { key: 'en-EN', label: 'English' },
-//             { key: 'pl-PL', label: 'Polski' },
-//         ];
-//         generalSettingsModelMock.appLangModel.translations = new Observable((observer) => observer.next(TEXTS_EN));
+    test('Should prepare items$ correctly', () => {
+        const expectedResult = [
+            { key: 'en-EN', label: 'English' },
+            { key: 'pl-PL', label: 'Polski' },
+        ];
+        appContextViewModelMok.translations$ = new Observable((observer) => observer.next(TEXTS_EN));
 
-//         testScheduler.run(({ expectObservable }) => {
-//             const model = new ChangeLangElementViewModel();
-//             expectObservable(model.items$).toBe('a', { a: expectedResult });
-//         });
-//     });
-// });
+        testScheduler.run(({ expectObservable }) => {
+            const model = new ChangeLangElementViewModel(appContextViewModelMok);
+            expectObservable(model.items$).toBe('a', { a: expectedResult });
+        });
+    });
+});
 
 export {};
