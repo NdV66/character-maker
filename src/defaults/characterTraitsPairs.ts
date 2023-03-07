0223a940ec79ec03cf844297dba481d666d4d175
Marta Zazlak <NdV66@users.noreply.github.com>
2023-03-07 15:33:29 +0100

    Features/new pairs (#5)
    - add new pairs

--- src/defaults/characterTraitsPairs.ts
+++ src/defaults/characterTraitsPairs.ts
@@ -1,5 +1,7 @@
 import { TCharacterTraitData } from '../types';
 
+// Happy <-> Melancholic
+
 export const CHARACTER_TRAITS_PAIRS: TCharacterTraitData[] = [
     {
         id: 'h_e',
@@ -15,5 +17,30 @@ export const CHARACTER_TRAITS_PAIRS: TCharacterTraitData[] = [
         id: 'd_r',
         name: 'DREAMER',
         opposite: 'REALIST',
+    },
+    {
+        id: 'l_e',
+        name: 'LOGICAL',
+        opposite: 'EMOTIONAL',
+    },
+    {
+        id: 'c_i',
+        name: 'CALM',
+        opposite: 'IMPULSIVE',
+    },
+    {
+        id: 'f_c',
+        name: 'FEARFUL',
+        opposite: 'COURAGEOUS',
+    },
+    {
+        id: 'l_h',
+        name: 'LOVING',
+        opposite: 'HATEFUL',
+    },
+    {
+        id: 'h_m',
+        name: 'HAPPY',
+        opposite: 'MELANCHOLIC',
     },
 ];
